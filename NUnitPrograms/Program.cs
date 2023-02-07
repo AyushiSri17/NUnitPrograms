72931f93ffdbc2b12c721d71380b1cdbd8704183
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-02-07 20:12:07 +0530

    [Ayushi] Added Vending Machine Program

--- NUnitPrograms/Program.cs
+++ NUnitPrograms/Program.cs
@@ -11,6 +11,21 @@ namespace NUnitPrograms
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to NUnit program");
+            Console.WriteLine("Choose the option for the program want to perform");
+            Console.WriteLine("1: Vending Machine");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    VendingMachine machine=new VendingMachine();
+                    Console.WriteLine("Enter the amount you want to withdraw");
+                    int amount=Convert.ToInt32(Console.ReadLine());
+                    machine.CountCurrency(amount);
+                    break;
+                default:
+                    Console.WriteLine("Choose the correct option");
+                    break;
+            }
             Console.ReadLine();
         }
     }
---
+++ NUnitPrograms/VendingMachine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitPrograms
+{
+    public class VendingMachine
+    {
+        public void CountCurrency(int amount)
+        {
+            int[] notes = new int[] { 2000, 500, 100, 50, 10, 5, 2, 1 };
+            int[] noteCounter = new int[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount % notes[i];
+                }
+            }
+            Console.WriteLine("Number of notes withdrawn");
+            for (int i = 0; i < 8; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine("{1} note of {0}",notes[i],noteCounter[i]);
+                }
+            }
+        }
+    }
+}
